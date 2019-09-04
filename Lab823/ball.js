4189c5a88943a2af11b0a495bb3bcff5e83b3dff
Sky <802526@seq.org>
2019-09-03 22:36:16 -0700

    Attraction and Repulsion done

--- Lab823/ball.js
+++ Lab823/ball.js
@@ -6,7 +6,7 @@ class Ball{
     this.loc = createVector(x, y);
     this.vel = createVector(dx, dy);
     this.acc = createVector(0,0);
-    this.clr = color(random(255), random(255), random(255));
+    this.clr = color(random(150, 200), random(100, 255), random(100, 255));
     this.sizeX = sizeX;
     this.sizeY = sizeY;
 
@@ -16,6 +16,7 @@ class Ball{
   // this.sizecontrol();
   this.update();
   this.render();
+  this.attraction();
 }
   checkEdges(){
     if(this.loc.x<this.sizeX/2){
@@ -37,12 +38,33 @@ class Ball{
   //   }
   // }
   update(){
-      this.loc.x=this.loc.x+this.vel.x;
-      this.loc.y=this.loc.y+this.vel.dy;
+      // this.loc.x=this.loc.x+this.vel.x;
+      // this.loc.y=this.loc.y+this.vel.y;
+      //this moves the balls
       // this.sizeX=this.sizeX+1
+      this.loc.add(this.vel);
+      this.vel.add(this.acc);
     }
   render(){
     fill(this.clr);
     ellipse(this.loc.x, this.loc.y, this.sizeX, this.sizeY)
   }
+  attraction(){
+var distToMainBall;
+distToMainBall = this.loc.dist(mainBall.loc);
+if(distToMainBall < 90){
+  this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+  this.acc.normalize();
+  this.acc.mult(1);
+}
+else if(distToMainBall < 250){
+  this.acc = p5.Vector.sub( mainBall.loc, this.loc);
+  this.acc.normalize();
+  this.acc.mult(0.1);
+}
+else{
+  this.acc = createVector(0,0);
+}
+  }
+
 }//end ball class
--- Lab823/sketch.js
+++ Lab823/sketch.js
@@ -1,6 +1,7 @@
 //Sky Gastinel
 //August 21st 2019
 var balls = []
+var mainBall
 function setup() {
   // put setup code here
   var cnv = createCanvas(800,800);
@@ -10,16 +11,22 @@ loadBalls(10);
 }
 
 function draw() {
+  background(0, 0, 0, 100)
   // put drawing code here
   // b1.run();
   // b2.run();
   // b3.run();
   runBalls();
+  mainBall.run();
 }
 function loadBalls(n){
+  fill(84, 46, 255)
+  mainBall = new Ball (width/2, height/2, 0, 0, 80, 80)
   for(var i = 0; i<n; i++){
     balls[i] = new Ball (random (width), random(height), random (-8,8), random(-8,8), random(10,20), random(10,20))
 //this is the ball constructor from before
+//function loadballs is called once at the beginning
+//the for loop basically creates each ball. starts at 0, changes by 1 up until 9 aka the n value.
   }
 }
 function runBalls(){
