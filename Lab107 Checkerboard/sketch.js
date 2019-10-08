1264618db2427c8d8ef7480636735822805c9f40
Sky <802526@seq.org>
2019-10-07 22:57:06 -0700

    Checkerboard
    Has all components except for the alternating colors

--- Lab107 Checkerboard/sketch.js
+++ Lab107 Checkerboard/sketch.js
@@ -1,21 +1,24 @@
-//  Your Name
-// 	Date or version number
-//  This is a comment
-//  The setup function function is called once when your program begins
+//Sky Gastinel
+//7 October 2019
+var numberRectV = 8
+var numberRectH = 8
+var a, b, c;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
-  loadSquares();
-
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].render;
-  }
-
+  fill(209, 209, 209);
 }
 
 //  The draw function is called @ 30 fps
-function loadSquares() {
 
+function draw() {
+  for(var r = 1; r <= numberRectH; r++) {
+    for(var n = 1; n <= numberRectV; n++) {
+      var upratio = (r-1)/(numberRectH-1);
+      var acrossratio = (n-1)/(numberRectV-1);
+      fill(20, 100, 20);
+      rect((n-1)*width/numberRectV, (r-1)*height/numberRectH, width/numberRectV, height/numberRectH);
+    }
+  }
 }
