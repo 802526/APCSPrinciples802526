dd807f374f4c8080f007690dbce22af7dfb1b434
Sky Gastinel <sgastinel@WS-ACADBUS.EDU>
2019-10-17 08:50:53 -0700

    insertion sorting

---
+++ Lab1017 Insertion Sorting/sketch.js
@@ -0,0 +1,19 @@
+//  Your Name
+// 	Date or version number
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+var list = []
+for (var n = 0; n<10001; n++) {
+  list[n] = random(1, 100);
+}
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
