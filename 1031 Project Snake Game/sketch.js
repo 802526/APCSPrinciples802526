e52f509fc54d9ea262c16dbf72991a71999c05f2
802526 <54591152+802526@users.noreply.github.com>
2019-11-01 08:45:12 -0700

    Snake Project Edit
    Snake now moves frame by frame and canvas is a different color

--- 1031 Project Snake Game/sketch.js
+++ 1031 Project Snake Game/sketch.js
@@ -6,16 +6,23 @@ snake = []
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
+  background(188, 235, 190);
   fill(114, 100, 100);
-  for(var i = 0; i<1; i++) {
-    snake[i] = new Snake (400, 400, 15, 15);
-  }
+  frameRate(10);
+  loadSnakes(1);
+  //loadFood(j);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  background(188, 235, 190);
   for(var i = 0; i<1; i++) {
     snake[i].run();
+  }
+}
+
+function loadSnakes(n) {
+  for(var i = 0; i<n; i++) {
+    snake[i] = new Snake (400, 400, 20, 20);
   }
 }
--- 1031 Project Snake Game/snake.js
+++ 1031 Project Snake Game/snake.js
@@ -3,7 +3,7 @@ class Snake {
     this.loc = createVector(x, y);
     this.w = w;
     this.h = h;
-    this.clr = color(100, 100, 100);
+    this.clr = color(58, 133, 82);
   }
 
 run() {
@@ -12,11 +12,36 @@ run() {
 }
 
 update() {
-  if
+  this.keyReleased();
+  //this.checkEdges();
 }
 
 render() {
   fill(this.clr);
   rect(this.loc.x, this.loc.y, this.w, this.h);
+}
+
+keyReleased() {
+  if(keyCode === UP_ARROW) {
+    this.loc.y = this.loc.y - this.h
+  }
+  if(keyCode === DOWN_ARROW) {
+    this.loc.y = this.loc.y + this.h
+  }
+  if(keyCode === LEFT_ARROW) {
+    this.loc.x = this.loc.x - this.w
+  }
+  if(keyCode === RIGHT_ARROW) {
+    this.loc.x = this.loc.x + this.w
+  }
+}
+
+checkEdges() {
+  if(this.loc.x > 800 ||
+     this.loc.x< 0 ||
+     this.loc.y> 800 ||
+     this.loc.y< 0) {
+       gameState = 3
+     }
 }
 }
