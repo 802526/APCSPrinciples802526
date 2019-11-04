77cdd22bcd6a426a07f1c6bc635e24945d3d2c63
802526 <54591152+802526@users.noreply.github.com>
2019-11-04 08:49:15 -0800

    snake game edits (still not working)

--- 1031 Project Snake Game/snake.js
+++ 1031 Project Snake Game/snake.js
@@ -6,48 +6,48 @@ class Snake {
     this.size = size;
     this.clr = color(58, 133, 82);
     for(var i=0; i<size; i++) {
-      this.segment[i] = createVector(x+i, y);
+      this.segments[i] = createVector(x+i, y);
     }
   }
 
-run() {
-  this.update();
-  this.render();
-}
-
-update() {
-  this.keyReleased();
-  //this.checkEdges();
-}
-
-render() {
-  fill(this.clr);
-  for(var i = 0; i<segments.length; i++){
-      rect(20*this.loc.x, 20*this.loc.y, this.w, this.h);
+  run() {
+    this.update();
+    this.render();
   }
-}
 
-keyReleased() {
-  if(keyCode === UP_ARROW) {
-    this.loc.y = this.loc.y - this.h
+  update() {
+    this.keyReleased();
+    //this.checkEdges();
   }
-  if(keyCode === DOWN_ARROW) {
-    this.loc.y = this.loc.y + this.h
-  }
-  if(keyCode === LEFT_ARROW) {
-    this.loc.x = this.loc.x - this.w
-  }
-  if(keyCode === RIGHT_ARROW) {
-    this.loc.x = this.loc.x + this.w
-  }
-}
 
-checkEdges() {
-  if(this.loc.x > 800 ||
-     this.loc.x< 0 ||
-     this.loc.y> 800 ||
-     this.loc.y< 0) {
+  render() {
+    fill(this.clr);
+    for(var i = 0; i<this.segments.length; i++){
+        rect(20*this.segments[i], 20*this.segments[i], this.w, this.h);
+    }
+  }
 
-     }
-}
+  keyReleased() {
+    if(keyCode === UP_ARROW) {
+      this.loc.y = this.loc.y - this.h
+    }
+    if(keyCode === DOWN_ARROW) {
+      this.loc.y = this.loc.y + this.h
+    }
+    if(keyCode === LEFT_ARROW) {
+      this.loc.x = this.loc.x - this.w
+    }
+    if(keyCode === RIGHT_ARROW) {
+      this.loc.x = this.loc.x + this.w
+    }
+  }
+
+  checkEdges() {
+    if(this.loc.x > 800 ||
+       this.loc.x< 0 ||
+       this.loc.y> 800 ||
+       this.loc.y< 0) {
+
+    }
+  }
 }
