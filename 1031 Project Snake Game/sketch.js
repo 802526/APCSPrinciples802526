f54161208a0a4100569b81ade7458330c7303dd8
802526 <54591152+802526@users.noreply.github.com>
2019-11-13 08:48:36 -0800

    Adding gamestates (does not currently work)

--- 1031 Project Snake Game/sketch.js
+++ 1031 Project Snake Game/sketch.js
@@ -4,6 +4,8 @@
 //  The setup function function is called once when your program begins
 var snake;
 var food;
+var gamestate = 1;
+var button;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
@@ -17,10 +19,58 @@ function setup() {
 //  The draw function is called @ 30 fps
 function draw() {
   background(190, 235, 190);
-    snake.run();
-    food.run();
+  if(gamestate === 1) {
+    startGame()
+  }else if (gameState === 2) {
+    playGame();
+  }else if (gameState === 3) {
+    endGame();
+  }
 }
 
+function startGame() {
+  fill(90, 125, 242);
+rect(50, 50, 700, 700);
+//blue box
+fill(237, 135, 255);
+textSize(50);
+textAlign(CENTER);
+textStyle(BOLD);
+text('~Snake Game~', 400, 300);
+//Paddle Game title
+rect(200, 500, 400, 100);
+//pink box for buttons
+fill(90, 125, 242);
+textSize(50);
+textAlign(CENTER);
+textStyle(BOLD);
+text('click to play', 400, 570);
+//"play"
+fill(44, 66, 138);
+textSize(15);
+textStyle(BOLD);
+text('by: Sky Gastinel', 115, 745);
+//by: Sky Gastinel
+mouseClicked();
+}
+
+function mouseClicked() {
+  if(mouseX >200 &&
+  mouseX<600 &&
+  mouseY> 500 &&
+  mouseY< 600) {
+    gameState = 2;
+  }
+}
+
+function playGame() {
+  snake.run();
+  food.run();
+}
+//
+// endGame() {
+//
+// }
 
 function loadSnakes(n) {
     snake = new Snake (20, 20, 20, 20, 1);
